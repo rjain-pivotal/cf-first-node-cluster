30ca3a8ed81f5b5b98dd709e61a54e44bc76c9ba
Rajesh Jain <rajesh.jain@pivotal.io>
2015-10-22 13:06:45 -0400

    First Node Cluster App

---
+++ app.js
@@ -0,0 +1,45 @@
+// Include the cluster module
+var cluster = require('cluster');
+
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+
+    // Count the machine's CPUs
+    //var cpuCount = require('os').cpus().length;
+    var cpuCount = 1
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+
+    // Listen for dying workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the dead worker, we're not sentimental
+        console.log('Worker %d died :(', worker.id);
+        cluster.fork();
+
+    });
+
+// Code to run if we're in a worker process
+} else {
+
+    // Include Express
+    var express = require('express');
+
+    // Create a new Express application
+    var app = express();
+
+    // Add a basic route – index page
+    app.get('/', function (request, response) {
+        console.log('Request to worker %d', cluster.worker.id);
+        response.send('Hello from Worker ' + cluster.worker.id);
+    });
+
+    // Bind to a port
+    app.listen(3000);
+    console.log('Worker %d running!', cluster.worker.id);
+
+}
+
